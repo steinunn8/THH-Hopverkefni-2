63d3e07a059d73838ceb7abcaca08ad2899e0768
Steinunn <stf7@hi.is>
2014-03-12 16:36:57 +0000

    Stokkur
    Smá update,

--- Deck.py
+++ Deck.py
@@ -4,6 +4,8 @@ import random
 class Deck:
 	def __init__(self):
 		self.deck = []
+
+	def fullDeck(self):
 		suit = ["H", "S", "D", "C"]
 		rank = range(1, 14)
 		for i in range(0, len(suit)):
@@ -16,6 +18,7 @@ class Deck:
 	
 	#Shows first card and then removes it from the deck	
 	def draw(self):
+		self.isEmpty()
 		return self.deck.pop(0)
 
 	#Shows first card in deck but does not remove it
@@ -28,24 +31,36 @@ class Deck:
 	def addLast(self,card):
 		self.deck.append(card)
 
+	def emptyDeck(self):
+		self.deck = []
+
+	def isEmpty(self):
+		return self.deck == []
+
+
 class Card(object):
-	def __init__(self, Suit, Rank, Up = False):
+	def __init__(self, Suit, Rank, Up = False, Left = False, Right = False):
 		self.suit = Suit
 		self.rank = Rank
 		self.up = Up
+		self.left = Left
+		self.right = Right
 
 	#When str() is used on Card, [X Y] will be the output 
 	#where X is the suit of the card and Y is the rank
 	def __str__(self):
 		return "["+self.suit+" "+str(self.rank)+"]"
 
+
+
 #Test deck
 hand = Deck()
 hand.shuffle()
-for i in range(0, 52):
+"""for i in range(0, 52):
 		print hand.show(i),
+class Tree(object):
+	def __init__(self, Height):
+		self.height = Height
 
-
-
-
+"""
 
