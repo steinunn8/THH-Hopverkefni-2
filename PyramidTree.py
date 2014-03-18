7428b837389272ac7021e065366c7693da2f3d9d
Kristin Fjola <kristinfjolato@gmail.com>
2014-03-18 14:14:07 +0000

    Edda

--- PyramidTree.py
+++ PyramidTree.py
@@ -1,24 +1,32 @@
 import Deck
 		
 class CardTree(object):		
-	def __init__(self, data):
+	def __init__(self, data, x=None, y=None):
 		self.left = None
 		self.right = None
 		self.data = data
 		self.rightParent = None
 		self.leftParent = None
+		self.x = x
+		self.y = y
 	
-	def insert_left(self, data):
+	def insert_left(self, data, list):
 		if self.left is None:
 			self.left = CardTree(data)
 			self.left.rightParent = self
+			self.left.x = self.x-80
+			self.left.y = self.y+100
+			list.append(self.left)
 		else:
-			self.left.insert_left(data)
+			self.left.insert_left(data,list)
 	
-	def insert_right(self, data, special=False):
+	def insert_right(self, data, list, special=False):
 		if self.right is None:
 			self.right = CardTree(data)
 			self.right.leftParent = self
+			self.right.x = self.x+80
+			self.right.y = self.y+100
+			list.append(self.right)
 			if(special):
 				parent = self
 				master = parent.rightParent
@@ -27,9 +35,9 @@ class CardTree(object):
 				self.right.rightParent = magic.left
 		else:
 			if(special):
-				self.right.insert_right(data,True)
+				self.right.insert_right(data,list,True)
 			else:
-				self.right.insert_right(data)
+				self.right.insert_right(data,list)
 	
 	def delete(self):
 		if self.rightParent is not None:
@@ -51,73 +59,59 @@ class Pyramid:
 		self.deck.fullDeck()
 		self.deck.shuffle()
 		
-		#testing, print cards in deck
-		print self.deck.show(0)
-		print self.deck.show(1)
-		print self.deck.show(2)
-		print self.deck.show(3)
-		print "same card:"
-		print self.deck.show(4)
-		print self.deck.show(5)
-		print "aetla ad eyda thessu:"
-		print self.deck.show(6)
-		print "same card 1:"
-		print self.deck.show(7)
-		print "same card 2:"
-		print self.deck.show(8)
-		print self.deck.show(9)
+		self.list = []
 		
-		self.root = CardTree(self.deck.draw())
+		print "spil i stokki:"
+		print deck.show(0)
+		print deck.show(1)
+		print deck.show(2)
+		print deck.show(3)
+		print deck.show(4)
+		print deck.show(5)
+		print deck.show(6)
+		print deck.show(7)
+		print deck.show(8)
+		print deck.show(9)
+		
+		self.root = CardTree(self.deck.draw(),450,80)
+		self.list.append(self.root)
 		
 		for i in range(0, self.height-1):
-			self.root.insert_left(self.deck.draw())
-			self.root.insert_right(self.deck.draw())
+			self.root.insert_left(self.deck.draw(),self.list)
+			self.root.insert_right(self.deck.draw(),self.list)
 		
-		#finish pyramid with magic (make a function tomorrow...)
+		self.finishPyramid()
+		
+	def getDeck(self):
+		return self.deck
+	
+	def finishPyramid(self):
 		for i in range(self.height-2, 0, -1):
 			if(i == self.height-2):
 				for j in range(0, i):
-					self.root.left.insert_right(self.deck.draw(),True)
+					self.root.left.insert_right(self.deck.draw(),self.list,True)
 			if(i == self.height-3):
 				for j in range(0, i):
-					self.root.left.left.insert_right(self.deck.draw(),True)
+					self.root.left.left.insert_right(self.deck.draw(),self.list,True)
 			if(i == self.height-4):
 				for j in range(0, i):
-					self.root.left.left.left.insert_right(self.deck.draw(),True)
+					self.root.left.left.left.insert_right(self.deck.draw(),self.list,True)
 			if(i == self.height-5):
 				for j in range(0, i):
-					self.root.left.left.left.left.insert_right(self.deck.draw(),True)
+					self.root.left.left.left.left.insert_right(self.deck.draw(),self.list,True)
 			if(i == self.height-6):
 				for j in range(0, i):
-					self.root.left.left.left.left.left.insert_right(self.deck.draw(),True)
-				
-		
-		#testing if the cards came out right
-		print self.root.show()
-		
-		print self.root.left.show()
-		print self.root.right.show()
-		
-		print self.root.left.left.show()
-		print self.root.right.right.show()
-		
-		print self.root.left.left.left.show()
-		
-		print "thessu verdur eytt bradum"
-		print self.root.right.right.right.show() 
-		self.root.right.right.right.delete()
-		print "and its gone:"
-		print self.root.right.right.right
-		
-		print "same card 1:"
-		print self.root.left.right.show()
-		print self.root.left.right.right.show()
-		print self.root.left.left.right.show()
-		
-		print "same card 2:"
-		print self.root.left.right.right.show()
+					self.root.left.left.left.left.left.insert_right(self.deck.draw(),self.list,True)
+	
+	def getCards(self):
+		return self.list
 		
 
 #test
 deck = Deck.Deck()
 pyramid = Pyramid(deck, 4)
+print "pyramidi"
+list = pyramid.getCards()
+for i in range(0, len(list)):
+	print list[i].data
+
