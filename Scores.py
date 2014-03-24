9e0c3cfc89f731b3c6d0823b98e89f66834c10b8
Jóhanna Agnes <johannaagma@gmail.com>
2014-03-24 11:49:56 +0000

    aðeins að laga í scores

--- Scores.py
+++ Scores.py
@@ -1,39 +1,52 @@
+#ekki ok
 def getScore(time, deckLength, pyramidLength, height, sortsOn, win):
 	timePoints = getTimePoints(time)
 	deckPoints = getDeckPoints(deckLength)
-	pyramidPoints = getPyrPoints(pyramidLength, height)
-	score = int(pyramidPoints + deckPoints + timePoints)
-	sortsOnMuliply = 3
-	winPoints = 10000
+	pyramidPoints = getPyrPoints(pyramidLength, height, sortsOn)
+	sortsOnMuliply = 2
+	winPoints = 1000
 	if(sortsOn):
 		if(win):
-			return score*height*sortsOnMuliply + winPoints
+			return int(sortsOnMuliply*(pyramidPoints + winPoints) + deckPoints + timePoints)
 		else:
-			return score*height*sortsOnMuliply
+			return int(sortsOnMuliply*pyramidPoints + deckPoints + timePoints)
 	else:
 		if(win):
-			return score*height + winPoints
+			return int(pyramidPoints + winPoints + deckPoints + timePoints)
 		else:
-			return score*height
-			
+			return int(pyramidPoints + deckPoints + timePoints)
+
+def getCurrentPoints(aGame):
+	pyramidLength = len(aGame.pyramid)
+	heigth = aGame.height
+	sortsOn = aGame.sortsOn
+	return getPyrPoints(pyramidLength, height, sortsOn)
+
+#ok	
 def getTimePoints(time):
 	timeBonus = 180  #3 minutes
 	if(time > timeBonus):
 		return 0
-	return (timeBonus-time)*100
-	
+	return (timeBonus-time)*10
+
+#ok	
 def getDeckPoints(deckLength):
 	return deckLength*100
 
-def getPyrPoints(pyramidLength, height):
+#ok
+def getPyrPoints(pyramidLength, height, sortsOn):
 	origPyrLength = getOrigPyramidLength(height)
+	if(sortsOn):
+		return (origPyrLength - pyramidLength)*1000*2
 	return (origPyrLength - pyramidLength)*1000
-	
+
+#ok	
 def getOrigPyramidLength(height, count=0):
 	if(height == 0):
 		return count
 	return getOrigPyramidLength(height-1, count+height)
-		
+
+#ok	
 def getHighScores():
 	scores = getScores()
 	highScores = []
@@ -48,11 +61,13 @@ def getHighScores():
 		scores.remove(top)
 	return highScores
 
+#ok
 def add(score):
 	scores = open('allScores.txt', 'a')
 	scores.write(score)	
 	scores.close()
-	
+
+#ok	
 def getScores():
 	temp = open('allScores.txt', 'r+')
 	str = temp.read()
