9df651b3a52762b5c4b6c6987b16d0a564af9cb4
Jóhanna Agnes <johannaagma@gmail.com>
2014-03-31 13:13:27 +0100

    comment á Scores og taka til í möppunni

--- Scores.py
+++ Scores.py
@@ -1,6 +1,6 @@
 import time, threading
 
-class score(object):
+class Score(object):
 	def __init__(self, aGame):
 		self.game = aGame
 		self.bonusTime = ''
@@ -9,21 +9,23 @@ class score(object):
 		self.bonus_minutes = 3
 		self.startBonusTime()
 		
-		
+	#returns the sum of all points for that game
 	def getScore(self):
 		timePoints = self.getTimePoints()
 		deckPoints = self.getDeckPoints()
 		pyramidPoints = self.getPyrPoints()
 		winPoints = self.getWinPoints()
 		return timePoints + deckPoints + pyramidPoints + winPoints
-				
+	
+	#returns a string that says "Score: " and the current score
 	def getCurrentPoints(self):
 		pyramidLength = len(self.game.pyramid)
 		height = self.game.height
 		sortsOn = self.game.sortsOn
 		points = "Score: " + str(self.getPyrPoints())
 		return points
-
+	
+	#returns the number of points you got for finishing the game early
 	def getTimePoints(self):
 		time = self.game.time
 		timeBonus = 180  #3 minutes
@@ -31,10 +33,12 @@ class score(object):
 			return 0
 		return int((timeBonus-time)*10)
 
+	#returns the number of points you got for not using all the cards in the Deck
 	def getDeckPoints(self):
 		deckLength = len(self.game.deck.deck)
 		return deckLength*100
 
+	#returns the number of points you got for removing cards off the pyramid
 	def getPyrPoints(self):
 		pyramidLength = len(self.game.pyramid)
 		height = self.game.height
@@ -45,6 +49,7 @@ class score(object):
 			return (origPyrLength - pyramidLength)*1000*sortsOnMultiply
 		return (origPyrLength - pyramidLength)*1000
 
+	#returns the number of points you got for winning the game
 	def getWinPoints(self):
 		sortsOnMultiply = 2
 		sortsOn = self.game.sortsOn
@@ -58,21 +63,25 @@ class score(object):
 		else:
 			return 0
 	
+	#returns the number of cards in a pyramid that has the height=height
 	def getOrigPyramidLength(self, height, count=0):
 		if(height == 0):
 			return count
 		return self.getOrigPyramidLength(height-1, count+height)
 
+	#adds the string score into a textfile
 	def addScore(self, score):
 		scores = open('allScores.txt', 'a')
 		scores.write(score)	
 		scores.close()
 
+	#adds the string name into a textfile
 	def addName(self, name):
 		scores = open('allScores.txt', 'a')
 		scores.write(name)	
 		scores.close()
 
+	#returns a matrix that has the all the scores and names, that is [[name1, score1],[name2, score2],...]
 	def getAllScores(self):
 		temp = open('allScores.txt', 'r+')
 		scores = []
@@ -86,6 +95,7 @@ class score(object):
 		temp.close()
 		return scores
 		
+	#returns a matrix with the top five scores and name with the score in a descending order
 	def getHighScores(self):
 		scores = self.getAllScores()
 		highScores = []
@@ -95,17 +105,18 @@ class score(object):
 			length = 5
 		sortedScores = sorted(scores, key=lambda score:score[1], reverse=True)
 		#sortedScores = sorted(scores, reverse=True)
-		
 		for i in range(0,length):
 			highScores.append(sortedScores[i])
 		return highScores
 
+	#returns a string that explains how the game works
 	def getHelp(self):
 		temp = open('help.txt', 'r')
 		string = temp.read()
 		temp.close()
 		return string
-		
+	
+	#returns a string with the top 5 scores and names with the score in a descending order
 	def getHighScoreString(self):
 		list = self.getHighScores()
 		for i in range(0, len(list)):
@@ -115,6 +126,7 @@ class score(object):
 			tempString = tempString + list[i]
 		return tempString
 	
+	#updated the string bonusTime so there can be a countdown (?)
 	def startBonusTime(self):
 		timer = threading.Timer(1.0, self.startBonusTime)
 		timer.start()
@@ -138,6 +150,7 @@ class score(object):
 			self.bonus_seconds = 60
 			self.bonus_minutes -= 1
 	
+	#returns a string that says 'Timebonus: ' and the time that is left on the time bonus
 	def getBonusTime(self):
 		return self.bonusTime
 
